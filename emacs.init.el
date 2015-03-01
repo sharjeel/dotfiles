f0607ea8776fcee307bcbe2147f80412db7b18b1
Sharjeel Ahmed Qureshi <sharjeel.qureshi@gmail.com>
2015-03-01 21:00:54 +0000

    Different theme for graphics and text terminals

--- emacs.init.el
+++ emacs.init.el
@@ -11,8 +11,10 @@
 (package-initialize)
 
 ;; Look & Feel (This should be copied into main init.el for safe loading)
-; (load-theme (quote monokai) nil nil)
-; (load-theme (quote solarized-dark) nil nil)
+; (if (display-graphic-p) 
+;     (load-theme (quote monokai) nil nil)
+;   (load-theme (quote solarized-dark) nil nil))
+
 
 (setq initial-scratch-message "")
 (setq inhibit-startup-message t)
