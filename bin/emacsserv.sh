7559915aa4d6d1f50cf141f30ecd8e1d2e62cb9a
sharjeel <sharjeel.qureshi@gmail.com>
2015-03-08 23:41:13 -0700

    Emacs singleton instance like subl aliased to e. e somefile opens somefile in an existing instance of emacs if avialable, otherwise creates an instance which is used for subsequent calls

---
+++ bin/emacsserv.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+# Used with emacsclient as alternate-editor. 
+# Tries to instantiate an emacs instance with server running if
+# DISPLAY is set. Otherwise spawns a normal emacs instance.
+if [ -n "$SSH_CLIENT" ]; then
+    emacs --daemon
+    exit
+fi
+
+if [ -n "$DISPLAY" ]; then
+    nohup emacs --eval '(server-start)' $* >/dev/null 2>&1 & 
+else
+    $EDITOR
+fi
+
