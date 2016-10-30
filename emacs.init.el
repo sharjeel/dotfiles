957306bd466edfb018d5067274b92c2a251b34f4
Sharjeel Ahmed Qureshi <sharjeel.qureshi@gmail.com>
2016-10-30 09:58:31 -0700

    Emacs update

--- emacs.init.el
+++ emacs.init.el
@@ -1,8 +1,15 @@
-;; Look & Feel (This should be copied into main init.el for safe loading)
+(if (display-graphic-p)
+    (setq theme-to-load 'sanityinc-tomorrow-eighties)
+  (setq theme-to-load 'sanityinc-tomorrow-eighties))
 
-;; (if (display-graphic-p)
-;;     (load-theme 'sanityinc-tomorrow-eighties nil nil)
-;;   (load-theme 'sanityinc-tomorrow-night nil nil))
+;; Look & Feel (This should be copied into main init.el for safe loading) 
+;; (load-theme theme-to-load nil nil)
+;; (if (daemonp)
+;;     (add-hook 'after-make-frame-functions
+;;               (lambda (frame)
+;;                 (select-frame frame)
+;;                 (load-theme theme-to-load t)))
+;;       (load-theme theme-to-load t))
 
 (ignore-errors
   (require 'color-theme-sanityinc-tomorrow))
@@ -23,7 +30,6 @@
 (setq scroll-step 1)                ; Smooth scrolling
 (setq mouse-wheel-follow-mouse 't)  ; scroll window under mouse
 (setq mouse-wheel-progressive-speed nil) ; don't accelerate scrolling
-(tabbar-mode 1)
 
 (ignore-errors
   (cond ((eq system-type 'windows-nt)
@@ -84,21 +90,45 @@
 (global-set-key "\C-w" 'backward-kill-word)
 (global-set-key "\C-x\C-k" 'kill-region)
 (global-set-key "\C-c\C-k" 'kill-region)
+; (global-set-key (kbd "C-M-w") (lambda () (interactive) (progn (kill-ring-save (region-beginning) (region-end)) (emamux:copy-kill-ring))))
+(defun emamux-copy-region ()
+  (interactive)
+  (if (region-active-p)
+      (progn
+	(kill-ring-save (region-beginning) (region-end))
+	(emamux:copy-kill-ring 0))))
+(global-set-key (kbd "C-M-w") 'emamux-copy-region)
 
 ;; Splitting screen should be simpler
 (global-set-key (kbd "C-x |") 'split-window-right)
 (global-set-key (kbd "C-x -") 'split-window-below)
 
-;; Move across windows
-(global-set-key (kbd "C-x <C-left>") 'windmove-left)
-(global-set-key (kbd "C-x <C-right>") 'windmove-right)
-(global-set-key (kbd "C-x <C-up>") 'windmove-up)
-(global-set-key (kbd "C-x <C-down>") 'windmove-down)
+;; tmux like keybindings
+(define-key input-decode-map "\e\eOA" [(meta up)])
+(define-key input-decode-map "\e\eOB" [(meta down)])
+(define-key input-decode-map "\e\eOD" [(meta left)])
+(define-key input-decode-map "\e\eOC" [(meta right)])
+(global-set-key [(meta up)] 'windmove-up)
+(global-set-key [(meta down)] 'windmove-down)
+(global-set-key [(meta left)] 'windmove-left)
+(global-set-key [(meta right)] 'windmove-right)
+
+(when (fboundp 'windmove-default-keybindings)
+  (windmove-default-keybindings))
+(global-set-key (kbd "C-c <down>") 'windmove-down)
+(global-set-key (kbd "C-c <up>") 'windmove-up)
+(global-set-key (kbd "C-c <left>") 'windmove-left)
+(global-set-key (kbd "C-c <right>") 'windmove-right)
 
 ;; Ace-jump
 (when (require 'ace-jump-mode nil 'no-error)
+  (global-set-key (kbd "<f1>") 'ace-jump-mode)
+  (global-set-key (kbd "<f2>") 'ace-jump-mode)
   (global-set-key (kbd "C-c SPC") 'ace-jump-mode)
-  (global-set-key (kbd "C-x SPC") 'ace-jump-mode-pop-mark))
+  (global-set-key (kbd "M-s s") 'ace-jump-mode)
+  (global-set-key (kbd "M-s M-s") 'ace-jump-mode)
+  (global-set-key (kbd "C-x SPC") 'ace-jump-mode-pop-mark)
+  (global-set-key (kbd "M-s x") 'ace-jump-mode-pop-mark))
 
 ;; Multiple cursors
 (when (require 'multiple-cursors nil 'no-error)
@@ -113,6 +143,7 @@
   (key-chord-mode 1)
   (key-chord-define-global "`]" 'yank)
   (key-chord-define-global "cv" 'yank)
+  (key-chord-define-global "uy" (insert "uytfe"))
   (key-chord-define-global "xj" 'smex)
   (key-chord-define-global "xo" 'other-window)
   (key-chord-define-global "xs" 'save-some-buffers)
@@ -144,6 +175,8 @@
 ;; powerline, if available
 (add-to-list 'load-path "~/.emacs.d/vendor/emacs-powerline")
 (when (require 'powerline nil 'no-error)
+  (powerline-default-theme)
+  (scroll-bar-mode -1)
   (custom-set-faces
  '(mode-line ((t (:foreground "#030303" :background "#bdbdbd" :box nil))))
  '(mode-line-inactive ((t (:foreground "#f9f9f9" :background "#666666" :box nil))))))
@@ -151,59 +184,63 @@
 ;; Automatically start server if in graphical mode
 (if (display-graphic-p)  
     (server-start))
-; Confirm before exiting if server is running
+;; Confirm before exiting if server is running
 (add-hook 'kill-emacs-query-functions
 	  (lambda () (if (and (fboundp 'server-running-p) (server-running-p) )
 			 (y-or-n-p "Server is running. Do you really want to quit?")
 		       t)))
 
+(custom-set-variables 
+  ;; Temporary files which are cleaned up in emacs client sessions
+  '(server-temp-file-regexp "^/tmp/Re\\|/draft$\\|COMMIT_EDITMSG$"))
+
+
 ;; Automatically refresh reload unmodified buffers when buffers get changed
 (global-auto-revert-mode t)
 
 ;; evil
-(when (require 'evil nil 'no-error)
-  (define-key evil-normal-state-map "\C-e" 'move-end-of-line)
-  (define-key evil-insert-state-map "\C-e" 'move-end-of-line)
-  (define-key evil-visual-state-map "\C-e" 'move-end-of-line)
-  (define-key evil-motion-state-map "\C-e" 'move-end-of-line)
-  (define-key evil-normal-state-map "\C-f" 'forward-char)
-  (define-key evil-insert-state-map "\C-f" 'forward-char)
-  (define-key evil-insert-state-map "\C-f" 'forward-char)
-  (define-key evil-normal-state-map "\C-b" 'backward-char)
-  (define-key evil-insert-state-map "\C-b" 'backward-char)
-  (define-key evil-visual-state-map "\C-b" 'backward-char)
-  (define-key evil-normal-state-map "\C-d" 'delete-char)
-  (define-key evil-insert-state-map "\C-d" 'delete-char)
-  (define-key evil-visual-state-map "\C-d" 'delete-char)
-  (define-key evil-normal-state-map "\C-n" 'next-line)
-  (define-key evil-insert-state-map "\C-n" 'next-line)
-  (define-key evil-visual-state-map "\C-n" 'next-line)
-  (define-key evil-normal-state-map "\C-p" 'previous-line)
-  (define-key evil-insert-state-map "\C-p" 'previous-line)
-  (define-key evil-visual-state-map "\C-p" 'previous-line)
-  (define-key evil-normal-state-map "\C-w" 'delete)
-  (define-key evil-insert-state-map "\C-w" 'delete)
-  (define-key evil-visual-state-map "\C-w" 'delete)
-  (define-key evil-normal-state-map "\C-y" 'yank)
-  (define-key evil-insert-state-map "\C-y" 'yank)
-  (define-key evil-visual-state-map "\C-y" 'yank)
-  (define-key evil-normal-state-map "\C-k" 'kill-line)
-  (define-key evil-insert-state-map "\C-k" 'kill-line)
-  (define-key evil-visual-state-map "\C-k" 'kill-line)
-  (define-key evil-normal-state-map "Q" 'call-last-kbd-macro)
-  (define-key evil-visual-state-map "Q" 'call-last-kbd-macro)
-  (define-key evil-normal-state-map (kbd "TAB") 'evil-undefine)
-  (define-key evil-insert-state-map "\C-z" 'evil-normal-state)
+;; (when (require 'evil nil 'no-error)
+;;   (define-key evil-normal-state-map "\C-e" 'move-end-of-line)
+;;   (define-key evil-insert-state-map "\C-e" 'move-end-of-line)
+;;   (define-key evil-visual-state-map "\C-e" 'move-end-of-line)
+;;   (define-key evil-motion-state-map "\C-e" 'move-end-of-line)
+;;   (define-key evil-normal-state-map "\C-f" 'forward-char)
+;;   (define-key evil-insert-state-map "\C-f" 'forward-char)
+;;   (define-key evil-insert-state-map "\C-f" 'forward-char)
+;;   (define-key evil-normal-state-map "\C-b" 'backward-char)
+;;   (define-key evil-insert-state-map "\C-b" 'backward-char)
+;;   (define-key evil-visual-state-map "\C-b" 'backward-char)
+;;   (define-key evil-normal-state-map "\C-d" 'delete-char)
+;;   (define-key evil-insert-state-map "\C-d" 'delete-char)
+;;   (define-key evil-visual-state-map "\C-d" 'delete-char)
+;;   (define-key evil-normal-state-map "\C-n" 'next-line)
+;;   (define-key evil-insert-state-map "\C-n" 'next-line)
+;;   (define-key evil-visual-state-map "\C-n" 'next-line)
+;;   (define-key evil-normal-state-map "\C-p" 'previous-line)
+;;   (define-key evil-insert-state-map "\C-p" 'previous-line)
+;;   (define-key evil-visual-state-map "\C-p" 'previous-line)
+;;   (define-key evil-normal-state-map "\C-w" 'delete)
+;;   (define-key evil-insert-state-map "\C-w" 'delete)
+;;   (define-key evil-visual-state-map "\C-w" 'delete)
+;;   (define-key evil-normal-state-map "\C-y" 'yank)
+;;   (define-key evil-insert-state-map "\C-y" 'yank)
+;;   (define-key evil-visual-state-map "\C-y" 'yank)
+;;   (define-key evil-normal-state-map "\C-k" 'kill-line)
+;;   (define-key evil-insert-state-map "\C-k" 'kill-line)
+;;   (define-key evil-visual-state-map "\C-k" 'kill-line)
+;;   (define-key evil-normal-state-map "Q" 'call-last-kbd-macro)
+;;   (define-key evil-visual-state-map "Q" 'call-last-kbd-macro)
+;;   (define-key evil-normal-state-map (kbd "TAB") 'evil-undefine)
+;;   (define-key evil-insert-state-map "\C-z" 'evil-normal-state)
 
-  (defun evil-undefine ()
-    (interactive)
-    (let (evil-mode-map-alist)
-      (call-interactively (key-binding (this-command-keys)))))
+;;   (defun evil-undefine ()
+;;     (interactive)
+;;     (let (evil-mode-map-alist)
+;;       (call-interactively (key-binding (this-command-keys)))))
 
-  (evil-mode 1)
-  (setq evil-move-cursor-back nil))
+;;   (evil-mode 1)
+;;   (setq evil-move-cursor-back nil))
 
 (defun myinit ()
   (interactive)
   (find-file "~/.personalconfig/emacs.init.el"))
-
